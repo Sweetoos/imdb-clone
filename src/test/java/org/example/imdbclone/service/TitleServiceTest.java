f89040ea06ae4fbe0e6153463ebdc94f6030ba2a
Sweetoos <thespbsik@gmail.com>
2026-05-21 23:46:53 +0200

    organized tests

--- src/test/java/org/example/imdbclone/service/TitleServiceTest.java
+++ src/test/java/org/example/imdbclone/service/TitleServiceTest.java
@@ -4,6 +4,8 @@ import org.example.imdbclone.model.Title;
 import org.example.imdbclone.model.TitleType;
 import org.example.imdbclone.repository.TitleRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -29,8 +31,8 @@ public class TitleServiceTest {
     private Title title;
 
     @BeforeEach
-    void setUp(){
-        title=new Title();
+    void setUp() {
+        title = new Title();
         title.setTitleId(99999999L);
         title.setTitleName("50 pozycji Lococka");
         title.setExplicitContent(true);
@@ -40,50 +42,70 @@ public class TitleServiceTest {
         title.setTitleType(TitleType.TVSERIES);
     }
 
-    @Test
-    void shouldCreateTitleSuccessfully() throws Exception {
-        Title titleToSave=Title.builder()
-                .titleName("50 pozycji Lococka")
-                .explicitContent(true)
-                .runtimeMinutes(69)
-                .startYear(2026)
-                .endYear(2026)
-                .titleType(TitleType.TVSERIES)
-                .build();
+    @Nested
+    @DisplayName("SERVICE: CREATE")
+    class CreateTitleTests {
+        @Test
+        void shouldCreateTitleSuccessfully() throws Exception {
+            Title titleToSave = Title.builder()
+                    .titleName("50 pozycji Lococka")
+                    .explicitContent(true)
+                    .runtimeMinutes(69)
+                    .startYear(2026)
+                    .endYear(2026)
+                    .titleType(TitleType.TVSERIES)
+                    .build();
 
-        when(titleRepository.save(any(Title.class))).thenReturn(title);
+            when(titleRepository.save(any(Title.class))).thenReturn(title);
 
-        Title result=titleService.createTitle(titleToSave);
+            Title result = titleService.createTitle(titleToSave);
 
-        assertNotNull(result.getTitleId(), "ID should not be null");
-        assertEquals(99999999L,result.getTitleId(), "ID should be 99999999");
+            assertNotNull(result.getTitleId(), "ID should not be null");
+            assertEquals(99999999L, result.getTitleId(), "ID should be 99999999");
 
-        assertEquals("50 pozycji Lococka", result.getTitleName());
-        verify(titleRepository, times(1)).save(titleToSave);
+            assertEquals("50 pozycji Lococka", result.getTitleName());
+            verify(titleRepository, times(1)).save(titleToSave);
+        }
     }
 
-    @Test
-    void shouldReturnTitleByIdSuccessfully() throws Exception {
-        when(titleRepository.findById(99999999L)).thenReturn(Optional.of(title));
+    @Nested
+    @DisplayName("SERVICE: READ")
+    class ReadTitleTests {
+        @Test
+        void shouldReturnTitleByIdSuccessfully() throws Exception {
+            when(titleRepository.findById(99999999L)).thenReturn(Optional.of(title));
 
-        Optional<Title> foundTitle=titleService.getTitleById(99999999L);
+            Optional<Title> foundTitle = titleService.getTitleById(99999999L);
 
-        assertThat(foundTitle).isPresent();
-        assertThat(foundTitle.get().getTitleId()).isEqualTo(99999999L);
+            assertThat(foundTitle).isPresent();
+            assertThat(foundTitle.get().getTitleId()).isEqualTo(99999999L);
+        }
+
+        @Test
+        void shouldReturnEmptyOptionalWhenTitleNotFound() throws Exception {
+            when(titleRepository.findById(99999999L)).thenReturn(Optional.empty());
+            Optional<Title> foundTitle = titleService.getTitleById(99999999L);
+
+            assertThat(foundTitle).isEmpty();
+            verify(titleRepository, never()).save(any(Title.class));
+        }
+
     }
 
-    @Test
-    void shouldReturnEmptyOptionalWhenTitleNotFound() throws Exception {
-        when(titleRepository.findById(99999999L)).thenReturn(Optional.empty());
-        Optional<Title> foundTitle=titleService.getTitleById(99999999L);
-
-        assertThat(foundTitle).isEmpty();
-        verify(titleRepository, never()).save(any(Title.class));
+    @Nested
+    @DisplayName("SERVICE: UPDATE")
+    class UpdateTitleTests {
+        @Test
+        void shouldUpdateTitleSuccessfully() throws Exception {
+            when(titleRepository.findById(99999999L)).thenReturn(Optional.of(title));
+            //TODO
+        }
     }
 
-    @Test
-    void shouldUpdateTitleSuccessfully() throws Exception {
-        when(titleRepository.findById(99999999L)).thenReturn(Optional.of(title));
-        //TODO
+
+    @Nested
+    @DisplayName("SERVICE: DELETE")
+    class DeleteTitleTests {
+
     }
 }
